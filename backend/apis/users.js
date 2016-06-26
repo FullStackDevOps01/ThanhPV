e61bb8cb3907ec66ba92ee1dda5497bd40ab818c
thanhpv <thanhpv@fimo.edu.vn>
2016-06-26 19:41:39 +0700

    Lesson4: Authentication

--- backend/apis/users.js
+++ backend/apis/users.js
@@ -1,11 +1,11 @@
-/**
- * Created by thanh on 19/6/2016.
- */
 'use strict';
 var express = require('express'),
     db = require('../models'),
-    router = express.Router(),
-    logger = require('../helpers/logger');
+    logger = require('../helpers/logger'),
+    moment = require('moment'),
+    config = require('config'),
+    crypto = require('crypto'),
+    router = express.Router();
 
 // create a new user
 router.post('/create', function(req, res){
@@ -14,15 +14,47 @@ router.post('/create', function(req, res){
         if (error) {
             return res.status(406).send(JSON.stringify({error}));
         }
-        delete new_user['salt'];
-        delete new_user['password'];
         res.send(JSON.stringify(new_user));
     });
 });
 
-router.get('/', function (req, res) {
-    logger.info('New request from %s to /', req.connection.remoteAddress);
-    res.send('test');
+// login
+router.post('/login', function(req, res){
+    var username = req.body.username;
+    var password = req.body.password;
+    db.User.findOne({
+        username: username
+    }).then(function(user){
+        if (!user.authenticate(password)) {
+            throw false;
+        }
+        db.Token.findOne({
+            username: username
+        }).then(function(t){
+            if (!t){
+                crypto.randomBytes(64, function(ex, buf) {
+                    var token = buf.toString('base64');
+                    var today = moment.utc();
+                    var tomorrow = moment(today).add(config.get('token_expire'), 'seconds').format(config.get('time_format'));
+                    token = new db.Token({
+                        username: username,
+                        token: token,
+                        expired_at: tomorrow.toString()
+                    });
+                    token.save(function(error, to){
+                        return res.send(JSON.stringify(to));
+                    });
+                });
+            }
+            res.send(JSON.stringify({
+                token: t.token,
+                id: user.id,
+                expired_at: t.expired_at
+            }));
+        });
+    }).catch(function(e){
+        res.status(401).send(JSON.stringify(e));
+    });
 });
 
 module.exports = router;
